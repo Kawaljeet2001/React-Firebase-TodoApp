8c2f53f4c7f1f915eb67bb221406e67bfbe3c7bd
Kawaljeet Singh Batra <batrakawaljeetsingh@gmail.com>
2021-03-27 22:21:26 +0530

    added the completed functionality

--- src/Task.js
+++ src/Task.js
@@ -6,6 +6,8 @@ import UpdateIcon from '@material-ui/icons/Update';
 
 const Task = ({ taskdata }) => {
 
+    const ref = React.useRef(null);
+
     const [task, settask] = React.useState(taskdata.task);
 
     const deletetask = () => {
@@ -21,12 +23,50 @@ const Task = ({ taskdata }) => {
         });
 
     }
+
+    const taskcompleted = () => {
+        
+        const db = firebase.firestore();
+        if(taskdata.iscompleted)
+        {
+            db.collection('tasks').doc(taskdata.id).update({
+                iscompleted: false
+            });
+
+            ref.current.style.textDecoration = '';
+            ref.current.style.opacity = '1'
+        }
+
+        else{
+            db.collection('tasks').doc(taskdata.id).update({
+                iscompleted: true
+            });
+            ref.current.style.textDecoration = 'line-through'
+            ref.current.style.opacity = '0.4'
+        }
+       
+
+    }
+
+    React.useEffect( () => {
+        if(taskdata.iscompleted)
+        {
+            ref.current.style.textDecoration = 'line-through'
+            ref.current.style.opacity = '0.4'
+        }
+
+        else
+        {
+            ref.current.style.textDecoration = '';
+            ref.current.style.opacity = '1'
+        }
+    }, [])
     return (
         <>
             <Container className = 'task'>
-                <Input className = 'task-field' multiline={true} disableUnderline={true} value={taskdata.task} onChange={(e) => { settask(e.target.value) }} value={task} />
+                <Input ref = {ref} className = 'task-field' multiline={true} disableUnderline={true} value={taskdata.task} onChange={(e) => { settask(e.target.value) }} value={task} />
                 <Tooltip className = 'tooltip' title = 'complete'>
-                    <Button style={{ maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px' }} variant='contained'>
+                    <Button onClick = {taskcompleted} style={{ maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px' }} variant='contained'>
                         <CheckIcon />
                     </Button>
                 </Tooltip>
